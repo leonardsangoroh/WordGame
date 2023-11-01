2eda2f7857edfd8dbf8d9409a0fd0417079c81e0
Leonard Sangoroh <61079370+leonardsangoroh@users.noreply.github.com>
2023-11-02 00:36:15 +0300

    Initial Commit

---
+++ WordGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WordGame
+//
+//  Created by Lee Sangoroh on 02/11/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
